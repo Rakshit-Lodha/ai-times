6ae13c0141188a30d71d1c4feefc7a11858d8b8d
rakshitlodha.business@gmail.com <rakshitlodha.business@gmail.com>
2026-01-25 10:03:07 +0530

    Article generation

--- update_monitor.py
+++ update_monitor.py
@@ -14,12 +14,12 @@ monitors = response.json()
 
 print(monitors)
 
-monitor_id_1 = ["monitor_7e43fb99ec844ccf983abc0ba82a4242"]
+# monitor_id_1 = ["monitor_7e43fb99ec844ccf983abc0ba82a4242"]
 
-for a in monitor_id_1:
-    response = requests.delete(
-        f"https://api.parallel.ai/v1alpha/monitors/{a}",
-        headers={"x-api-key": api_key}
-    )
+# for a in monitor_id_1:
+#     response = requests.delete(
+#         f"https://api.parallel.ai/v1alpha/monitors/{a}",
+#         headers={"x-api-key": api_key}
+#     )
 
-    print(f"Deleted: {response.status_code}")
+#     print(f"Deleted: {response.status_code}")
